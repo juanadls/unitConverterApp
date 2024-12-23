00dfb410981fb074350d7a1bcf36c81c25c732ef
juanadls <eleo.dev09@gmail.com>
2024-12-23 14:29:44 -0300

    added unit converter fun

--- app/src/main/java/com/example/unitcoverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitcoverterapp/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,11 +24,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnitCoverterAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding ->
+                   UnitConverter(modifier = innerPadding)
+
                 }
             }
         }
@@ -31,17 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+fun UnitConverter(modifier: PaddingValues = PaddingValues()){
+    Column {
+        Text("Unit converter")
+        OutlinedTextField(value = "Enter value", onValueChange = { })
+        Row {
+
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UnitCoverterAppTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
